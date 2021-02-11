13dac385c737a06ef056d8238b2afb190460b242
Mayra <mayra.pastor@est.fib.upc.edu>
2021-02-11 12:54:19 +0100

    email script

--- send_email.py
+++ send_email.py
@@ -2,13 +2,14 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-message = Mail(
-    from_email='mayra.pastor@estudiantat.upc.edu',
-    to_emails='pastorvaldivia.m@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+def send_it():
+    message = Mail(
+        from_email='mayra.pastor@estudiantat.upc.edu',
+        to_emails='pastorvaldivia.m@gmail.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
 
-#Using os won't work for Chrome extension...
-sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-response = sg.send(message)
-print(response.status_code, response.body, response.headers)
+    #Using os won't work for Chrome extension...
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code, response.body, response.headers)
