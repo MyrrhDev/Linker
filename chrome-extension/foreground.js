8c2f5c2edcf36c2ef76ff956fc8cf55de4908ad1
Mayra <mayra.pastor@est.fib.upc.edu>
2021-02-20 11:09:37 +0100

    Chrome extension sends email

--- chrome-extension/foreground.js
+++ chrome-extension/foreground.js
@@ -1,38 +1,3 @@
-// function UrlExists(url) {
-//     var http = new XMLHttpRequest();
-//     http.open('HEAD', url, false);
-//     http.send();
-//     return http.status;
-// }
-
-
-function UrlExists(url) {
-// if(XMLHttpRequest)
-// {
-  var request = new XMLHttpRequest();
-  if("withCredentials" in request)
-  {
-   // Firefox 3.5 and Safari 4
-   request.open('GET', url, true);
-//    request.onreadystatechange = handler;
-   request.send();
-  }
-  else if (XDomainRequest)
-  {
-   // IE8
-   var request = new XDomainRequest();
-   request.open("get", url);
-   request.send();
-
-   
-  }
-
- // This version of XHR does not support CORS
- // Handle accordingly
-// }
-    return request.status;
-}
-
 
 var link_array = [];
 var status_array = [];  
@@ -42,12 +7,15 @@ function usage(){
   for(var i=0; i<links.length; i++) {
     link_array.push(links[i].href);
     console.log(link_array)
-    status_array.push(UrlExists(links[i].href));
-    console.log(status_array)
+//     status_array.push(UrlExists(links[i].href));
+//     console.log(status_array)
   }
+  chrome.runtime.sendMessage({message: 'send email'});
+  console.log('I SENT THE MESSAGE')
 //   window.alert(link_array);
 }
 
 usage();
 console.log(link_array)
-console.log(status_array)
+// console.log(status_array)
+// send_mail();
