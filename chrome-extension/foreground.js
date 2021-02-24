460df4f5a0d5ee8659a150e541d6cd2f3634d449
Atreya Majumdar <atreyamaj@gmail.com>
2021-02-24 08:41:38 +0530

    Testing

--- chrome-extension/foreground.js
+++ chrome-extension/foreground.js
@@ -1,20 +1,22 @@
 
 var link_array = [];
 var status_array = [];  
-
+z
 function usage(){
   var links = document.links;
   for(var i=0; i<links.length; i++) {
     link_array.push(links[i].href);
-    console.log(link_array)
+    //console.log(link_array)
 //     status_array.push(UrlExists(links[i].href));
 //     console.log(status_array)
   }
-  chrome.storage.local.set({ "links": link_array });
-  chrome.runtime.sendMessage({message: 'send email'});  
-  console.log('sent links')
+  console.log(link_array)
+  chrome.runtime.sendMessage({message: 'send email'});
+  console.log('I SENT THE MESSAGE')
+//   window.alert(link_array);
 }
 
 usage();
 console.log(link_array)
 // console.log(status_array)
+// send_mail();
